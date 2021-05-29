3c5d159deabc4308081f9dcb380ee9f1551010b3
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-05-29 14:49:45 +0300

    feat: useAsync helper

---
+++ src/hooks/useAsync.ts
@@ -0,0 +1,28 @@
+import { useCallback, useState } from 'react'
+
+export function useAsync() {
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState<Error | undefined>()
+
+	const run = useCallback(
+		<
+			T = unknown,
+			P extends Array<unknown> = unknown[]
+		>(callback: (...args: P) => Promise<T> | T) => async (...args: P) => {
+			try {
+				await callback(...args)
+			} catch (error) {
+				setError(error)
+			} finally {
+				setLoading(false)
+			}
+		},
+		[],
+	)
+
+	return {
+		run,
+		loading,
+		error,
+	}
+}
