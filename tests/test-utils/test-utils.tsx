1b8289d66d79c28f65be99eaabd7b949921465f1
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-06-02 17:55:30 +0300

    types: filter testUtils by key

--- tests/test-utils/test-utils.tsx
+++ tests/test-utils/test-utils.tsx
@@ -1,7 +1,28 @@
 import { fireEvent, RenderResult } from '@testing-library/react'
 
-export function testUtils(testId: string, container: RenderResult) {
-	const utils = {
+interface Utils {
+	element: () => HTMLElement | null;
+	notExistsError: () => Error;
+	_element: () => HTMLElement;
+	click: () => boolean;
+	focus: () => boolean;
+	type: (value: string) => boolean;
+	text: () => string;
+}
+
+type UtilsKeys = keyof Utils
+
+interface TestUtilsOptions<K extends UtilsKeys> {
+	keys?: K[]
+}
+
+export function testUtils<K extends UtilsKeys>(
+	testId: string,
+	container: RenderResult,
+	options?: TestUtilsOptions<K>,
+) {
+	const { keys } = options || {}
+	const utils: Utils = {
 		element: () => container.queryByTestId(testId),
 		notExistsError: () => new Error('Element does not exists: ' + testId),
 		_element: () => {
@@ -14,9 +35,20 @@ export function testUtils(testId: string, container: RenderResult) {
 		},
 		click: () => fireEvent.click(utils._element()),
 		focus: () => fireEvent.focusIn(utils._element()),
+		text: () => utils._element().innerText,
 		type: (value: string) =>
 			fireEvent.input(utils._element(), { target: { value } }),
 	}
 
-	return utils
+	if (keys) {
+		const filteredUtils = Object.fromEntries(
+			Object.entries(utils).filter(
+				([ key ]) => ([ ...keys, 'element' ] as K[]).includes(key as K),
+			),
+		) as Pick<Utils, K | 'element'>
+
+		return filteredUtils
+	} else {
+		return utils
+	}
 }
