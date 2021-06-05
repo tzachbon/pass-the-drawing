2571e95122dc7ad5dcc791b422217b4825c716c7
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-06-05 15:43:15 +0300

    test: cleanup

--- tests/test-setup.tsx
+++ tests/test-setup.tsx
@@ -1,4 +1,13 @@
 import './extends/toBeCalledWithInstance'
 import { enableFetchMocks } from 'jest-fetch-mock'
+import { cleanup as firebaseCleanup } from './__mocks__/firebase'
+import { cleanup as routerCleanup } from './__mocks__/react-router-dom'
 
 enableFetchMocks()
+
+afterEach(() => {
+	void firebaseCleanup()
+	void routerCleanup()
+
+	jest.clearAllMocks()
+})
