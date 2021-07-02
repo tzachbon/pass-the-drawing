edd484944d339ee90ee61fc8793e89d392da0050
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-07-02 19:27:07 +0300

    Force yarn

---
+++ scripts/forceYarn.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+if (!process.env.npm_execpath.includes('yarn')) {
+  console.error('You must use Yarn to install dependencies:');
+  console.error('$ yarn install');
+  console.error('');
+  process.exit(1);
+}
