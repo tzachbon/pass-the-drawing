bed8ebb40c1294c11f43443b1e295a324f93c700
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-07-03 14:00:35 +0300

    Fix SelectSubject tests

--- src/components/SelectSubject/SelectSubject.test.tsx
+++ src/components/SelectSubject/SelectSubject.test.tsx
@@ -1,4 +1,5 @@
 import { GameSubjects } from '@constants'
+import { toTitleCase } from '@utils/toTitleCase'
 import { selectSubjectDriver } from './SelectSubject.driver'
 
 describe('SelectSubject', () => {
@@ -6,16 +7,9 @@ describe('SelectSubject', () => {
 
 	const setSubject = jest
 		.fn()
-		.mockImplementation((aSubject: string | undefined) => {
-			subject = aSubject
-		})
+		.mockImplementation((aSubject: string | undefined) => { subject = aSubject })
 
-	const driver = selectSubjectDriver({
-		props: {
-			subject,
-			setSubject,
-		},
-	}).beforeAndAfter()
+	const driver = selectSubjectDriver({ props: { subject, setSubject } }).beforeAndAfter()
 
 	beforeEach(() => {
 		subject = undefined
@@ -25,22 +19,20 @@ describe('SelectSubject', () => {
 		driver.testkit().input().focus()
 
 		for (const subjectValue of Object.values(GameSubjects)) {
-			expect(
-				driver.testkit().subject(subjectValue).element(),
-			).toHaveTextContent(subjectValue)
+			expect(driver.testkit().subject(subjectValue).text()).toEqual(toTitleCase(subjectValue))
 		}
 	})
 
 	it('should filter subject according to text', () => {
 		subject = 'food'
+		const expectedSubject = GameSubjects.Food
+		const expectedMissingSubject = GameSubjects.Superheroes
 		driver.withProps({ subject }).render()
 
 		driver.testkit().input().focus()
 
-		expect(
-			driver.testkit().subject(GameSubjects.Food).element(),
-		).toBeDefined()
-		expect(driver.testkit().subject(GameSubjects.Food).element()).toBeNull()
+		expect(driver.testkit().subject(expectedSubject).element()).toBeDefined()
+		expect(driver.testkit().subject(expectedMissingSubject).element()).toBeNull()
 	})
 
 	it('should call setSubject when clicking item', () => {
